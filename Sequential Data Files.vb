5aafc996160fc38f8bf11b3b63a4f71366251c57
Ryan Roque <34800562+Ryanroque1738@users.noreply.github.com>
2018-01-16 21:13:02 -0500

    Add files via upload

---
+++ Sequential Data Files.vb
@@ -0,0 +1,28 @@
+﻿Imports System.IO
+
+Module Module1
+
+    Sub Main()
+
+        Dim decSales As Decimal = 0
+
+        Dim inputFile As StreamReader
+
+        inputFile = File.OpenText("Sales.dat")
+
+        Console.WriteLine("Sales from file sales.dat")
+
+        Do Until inputFile.Peek = -1
+            decSales = CDec(inputFile.ReadLine())
+            Console.WriteLine(decSales)
+        Loop
+
+        inputFile.Close()
+
+        Console.WriteLine()
+        Console.Write("Press the enter key to terminate the program")
+        Console.Read()
+
+    End Sub
+
+End Module
